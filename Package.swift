fbf2f840abdfb364bda95a976efe2f7720506d2d
Luciano Perez Rocha <luciano@looping.com.uy>
2021-07-21 14:35:53 -0300

    updated refs.

--- Package.swift
+++ Package.swift
@@ -6,15 +6,11 @@ import PackageDescription
 let package = Package(
     name: "Moya-ObjectMapper",
     products: [
-        .library(name: "Moya-ObjectMapper", targets: ["Moya-ObjectMapper"]),
-        .library(name: "Moya-RxSwift-ObjectMapper", targets: ["Moya-RxSwift-ObjectMapper"]),
-        .library(name: "Moya-ReactiveSwift-ObjectMapper", targets: ["Moya-ReactiveSwift-ObjectMapper"])
+        .library(name: "Moya-ObjectMapper", targets: ["Moya-ObjectMapper"])
     ],
     dependencies: [
-        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "12.0.0")),
-        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "3.4.0")),
-        .package(url: "https://github.com/ReactiveX/RxSwift.git", .upToNextMajor(from: "4.5.0")),
-        .package(url: "https://github.com/ReactiveCocoa/ReactiveSwift.git", .upToNextMajor(from: "4.0.0"))
+        .package(url: "https://github.com/Moya/Moya.git", .upToNextMajor(from: "14.0.0")),
+        .package(url: "https://github.com/tristanhimmelman/ObjectMapper.git", .upToNextMajor(from: "4.1.0"))
     ],
     targets: [
         .target(
@@ -23,26 +19,6 @@ let package = Package(
                 "Moya",
                 "ObjectMapper"],
             path: "Source/Core",
-            exclude: [
-                "Sample"]),
-        .target(
-            name: "Moya-RxSwift-ObjectMapper",
-            dependencies: [
-                "Moya",
-                "RxMoya",
-                "ObjectMapper",
-                "RxSwift"],
-            path: "Source/RxSwift",
-            exclude: [
-                "Sample"]),
-        .target(
-            name: "Moya-ReactiveSwift-ObjectMapper",
-            dependencies: [
-                "Moya",
-                "ReactiveMoya",
-                "ObjectMapper",
-                "ReactiveSwift"],
-            path: "Source/ReactiveSwift",
             exclude: [
                 "Sample"])
     ]
